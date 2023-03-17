69389756f3bbb4aa1dca9bf1e6570ce0a4641f45
Arthur Kozhemyakin <trable228@icloud.com>
2023-03-17 21:37:14 +0300

    start screen done

--- app/src/main/java/com/example/testquizapp/MainActivity.kt
+++ app/src/main/java/com/example/testquizapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.testquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
