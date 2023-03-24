2b3bdd74332089b3061681ffaa618dfff419a3d7
Arthur Kozhemyakin <trable228@icloud.com>
2023-03-25 01:05:51 +0300

    questions src done

--- app/src/main/java/com/example/testquizapp/MainActivity.kt
+++ app/src/main/java/com/example/testquizapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.testquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.example.testquizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnStart.setOnClickListener {
+            if (binding.etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/testquizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Questions size", "${questionsList.size}")
+    }
+}
