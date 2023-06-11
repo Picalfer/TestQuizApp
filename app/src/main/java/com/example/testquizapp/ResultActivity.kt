ac65c5993443c8aef9a7a06552ed9f31793c862d
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-11 10:14:17 +0300

    refactor, screenshots added

--- app/src/main/java/com/example/testquizapp/ResultActivity.kt
+++ app/src/main/java/com/example/testquizapp/ResultActivity.kt
@@ -1,10 +1,8 @@
 package com.example.testquizapp
 
-import android.app.Instrumentation.ActivityResult
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.testquizapp.databinding.ActivityQuizQuestionsBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.example.testquizapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
@@ -12,20 +10,20 @@ class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityResultBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        binding = ActivityResultBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        val userName = intent.getStringExtra(Constants.USER_NAME)
-        binding.tvName.text = userName
-        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
-        val correctQuestions = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+        binding.apply {
+            val userName = intent.getStringExtra(Constants.USER_NAME)
+            tvName.text = userName
+            val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+            val correctQuestions = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
 
-        binding.tvScore.text = "Your score is $correctQuestions out of $totalQuestions"
+            tvScore.text = getString(R.string.score_text, correctQuestions, totalQuestions)
 
-        binding.btnSubmit.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            btnSubmit.setOnClickListener {
+                startActivity(Intent(this@ResultActivity, MainActivity::class.java))
+                finish()
+            }
         }
     }
 }
